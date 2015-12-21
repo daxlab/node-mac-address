e603f1db850d899a585783f247eba3329e6afae7
daxlab <mandeep25894@gmail.com>
2015-12-22 01:08:48 +0530

    added linux part

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by daxlab on 21/12/15.
+ */
+
+'use strict';
+var exec = require('child_process').exec;
+if (process.platform === 'darwin') {
+    //module.exports = require('./lib/osx');
+} else if (process.platform === 'win32') {
+    //module.exports = require('./lib/win');
+} else {
+    //module.exports = require('./lib/linux');
+    var util = {};
+    util.getMAC = function(cb) {
+            exec('ifconfig | grep HWaddr', function (err, stdout, stderr) {
+                if (!err) {
+                    var temp = stdout.split(' ');
+                    var MAC = temp[temp.length - 3];
+                    return cb(null, MAC);
+                }
+                else {
+                    console.log('Some error occurred in getting MAC.');
+                    return cb(err, null);
+                }
+            });
+    };
+    module.exports = util;
+}
